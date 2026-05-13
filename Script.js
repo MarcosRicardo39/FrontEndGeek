ad141e7f24b74b901e129073f6c32b1361698355
MarcosRicardo39 <vinigavea39@outlook.com>
2026-05-13 17:47:13 -0300

    Inclusão dos end points restantes e configuração html

--- Script.js
+++ Script.js
@@ -17,3 +17,63 @@ async function buscarProdutos() {
         `;
     });
 }// JavaScript source code
+
+async function adicionarProduto() {
+
+    const nome = document.getElementById("nomeProduto").value;
+    const preco = document.getElementById("precoProduto").value;
+
+    const novoProduto = {
+        nome: nome,
+        preco: parseFloat(preco)
+    };
+
+    const response = await fetch("https://localhost:7141/api/ProdutosGeek", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(novoProduto)
+    });
+
+    const dados = await response.json();
+
+    console.log(dados);
+
+    buscarProdutos();
+}
+
+async function atualizarProduto() {
+
+    const id = document.getElementById("idAtualizar").value;
+
+    const produtoAtualizado = {
+        id: parseInt(id),
+        nome: document.getElementById("nomeAtualizar").value,
+        preco: parseFloat(document.getElementById("precoAtualizar").value)
+    };
+
+    await fetch(`https://localhost:7141/api/ProdutosGeek/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(produtoAtualizado)
+    });
+
+    buscarProdutos();
+}
+
+
+async function deletarProduto() {
+
+    const id = document.getElementById("idDeletar").value;
+
+    await fetch(`https://localhost:7141/api/ProdutosGeek/${id}`, {
+        method: "DELETE"
+    });
+
+    console.log("Produto removido");
+
+    buscarProdutos();
+}
